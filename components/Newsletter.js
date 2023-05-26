e782c403f08d1273ae914838ad20d008ea924fe6
wangdawei <wangdawei@zuoyebang.com>
2023-05-26 14:51:36 +0800

    去掉 react-hook-form 的使用

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import { useState } from 'react'
 
-const Newsletter = () => {
-  const [status, setStatus] = React.useState(null);
-  const { register, handleSubmit, errors } = useForm();
+export default function Newsletter() {
+  const [email, setEmail] = useState('')
+  const [status, setStatus] = useState('')
 
-  const onSubmit = async (data) => {
+  const handleSubmit = async event => {
+    event.preventDefault()
+    setStatus('Sending...')
     try {
-      const response = await axios.post('/api/newsletter', data);
-      setStatus(response.data.message);
+      const response = await fetch('/api/newsletter', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+      })
+      const data = await response.json()
+      setStatus(data.message)
+      setEmail('')
     } catch (error) {
-      setStatus('An error occurred, please try again.');
+      setStatus('Oops, something went wrong. Please try again later.')
+      console.error(error)
     }
-  };
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" ref={register({ required: true })} />
-        {errors.email && <span>This field is required</span>}
-        <button type="submit">Subscribe</button>
+    <div className="bg-gray-100 p-8 rounded-xl">
+      <h2 className="text-xl font-bold mb-4">Subscribe to our newsletter</h2>
+      <form onSubmit={handleSubmit}>
+        <label className="sr-only" htmlFor="email">
+          Email address
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          className="border-gray-300 border-2 p-2 rounded-lg w-full mb-4"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
+          disabled={status === 'Sending...'}
+          >
+          {status === 'Sending...' ? 'Sending...' : 'Subscribe'}
+        </button>
       </form>
-      {status && <p>{status}</p>}
+      {status && <p className="mt-4">{status}</p>}
     </div>
-    );
-};
+    )
+}
